b5062b3db807c23ae964e4d80573dafb02630813
Dan Grund <hello@dangrund.com>
2020-11-15 21:19:34 -0700

    Update mac-setup.sh

--- mac-setup.sh
+++ mac-setup.sh
@@ -59,6 +59,9 @@ brew cask install iterm2
 echo "Installing oh-my-zsh..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
 # Install Git
 if test ! $(which git); then
     echo "Installing Git..."
